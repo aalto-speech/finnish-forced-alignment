2d40d4cc6f720594a927e31477743cff3cd82613
juholeinonen <juho.leinonen@aalto.fi>
2021-01-04 16:46:40 +0200

    Added parser, trying to modulize code

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -6,40 +6,50 @@ import pandas as pd
 from wer import wer  # TODO Does this work outside pycharm?
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+from scipy.stats import percentileofscore
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Calculate metrics from two ctms, first one gold standard')
+    parser.add_argument('gold', type=str,
+                    help='The gold standard ctm, which to compare to')
+    parser.add_argument('created', type=str,
+                    help='The created ctm, which are evaluated')
+    parser.add_argument('name', type=str,
+                    help='Root name for the generated files')
+    args = parser.parse_args()
+    return args
 
 
 # Read the ctms to a dataframe
-column_names = ["Filename", "segment", "start", "duration", "token"]
-gold_ctms_file = "../data/test-61-70970-clean_hires-ctm_no_frame_def_par-ctm"
-gold_ctms_df = pd.read_csv(gold_ctms_file, sep=' ', names=column_names, index_col=False, engine='python')
+def create_ctm_dfs(gold_ctms_file, created_ctms_file):
+    column_names = ["Filename", "segment", "start", "duration", "token"]
 
-created_ctms_file = "../data/2020_12_14_06_55_PM_ctm"
-created_ctms_df = pd.read_csv(created_ctms_file, sep=' ', names=column_names, index_col=False, engine='python')
+    gold_ctms_df = pd.read_csv(gold_ctms_file, sep=' ', names=column_names, index_col=False, engine='python')
+    created_ctms_df = pd.read_csv(created_ctms_file, sep=' ', names=column_names, index_col=False, engine='python')
 
-# Create end-time column
-'''df['c'] = df.apply(lambda row: row.a + row.b, axis=1)
-If you get the SettingWithCopyWarning you can do it this way also:
+    # Create end-time column
+    '''df['c'] = df.apply(lambda row: row.a + row.b, axis=1)
+    If you get the SettingWithCopyWarning you can do it this way also:
 
-fn = lambda row: row.a + row.b # define a function for the new column
-col = df.apply(fn, axis=1) # get column data with an index
-df = df.assign(c=col.values) # assign values to column 'c'
-'''
-gold_ctms_df["end"] = gold_ctms_df.apply(lambda row: row.start + row.duration, axis=1)
-created_ctms_df["end"] = created_ctms_df.apply(lambda row: row.start + row.duration, axis=1)
+    fn = lambda row: row.a + row.b # define a function for the new column
+    col = df.apply(fn, axis=1) # get column data with an index
+    df = df.assign(c=col.values) # assign values to column 'c'
+    '''
+    gold_ctms_df["end"] = gold_ctms_df.apply(lambda row: row.start + row.duration, axis=1)
+    created_ctms_df["end"] = created_ctms_df.apply(lambda row: row.start + row.duration, axis=1)
+
+    return gold_ctms_df, created_ctms_df
+
+
 
 # Initialize frame counts and get list of files.
-
 # Iterate file by file
 list_of_filenames = gold_ctms_df["Filename"].unique().tolist()
 number_of_files = len(list_of_filenames)
 
-slow_total_correct_empty_frames = []
-slow_total_correct_token_frames = []
-slow_total_incorrect_token_frames = []
-slow_total_number_of_frames = []
-
 # Faster method of doing frame-by-frame comparison
-
 fast_total_correct_empty_frames = []
 fast_total_correct_token_frames = []
 fast_total_incorrect_token_frames = []
@@ -115,39 +125,45 @@ for filename in list_of_filenames:
             break
 
 # STATISTICS
-slow_correct_empty_np_arr = np.asarray(slow_total_correct_empty_frames)
-slow_correct_token_np_arr = np.asarray(slow_total_correct_token_frames)
-slow_incorrect_np_arr = np.asarray(slow_total_incorrect_token_frames)
+fast_correct_empty_np_arr = np.asarray(fast_total_correct_empty_frames)
+fast_correct_token_np_arr = np.asarray(fast_total_correct_token_frames)
+fast_incorrect_np_arr = np.asarray(fast_total_incorrect_token_frames)
 
 difference_np_arr = np.asarray(ctm_mistakes_seconds)
 
-slow_correct_mean = np.mean(slow_correct_token_np_arr)
-slow_correct_median = np.median(slow_correct_token_np_arr)
-slow_correct_max = np.max(slow_correct_token_np_arr)
-slow_correct_min = np.min(slow_correct_token_np_arr)
+# Median start difference
+start_difference_median = np.median(difference_np_arr[:, 0])
+
+# What percentage of tokens are inside 40ms of actual start
+percentileofscore_of_40ms_start = percentileofscore(np.abs(difference_np_arr[:, 0]), 0.04)
+
+# What percentage of tokens are inside 40ms of actual end
+percentileofscore_of_40ms_end = percentileofscore(np.abs(difference_np_arr[:, 1]), 0.04)
 
 fig1, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True)
 ax1.set_title('Token')
 ax2.set_title('Empty')
 ax3.set_title('Wrong')
-ax1.boxplot(slow_correct_token_np_arr)
-ax2.boxplot(slow_correct_empty_np_arr)
-ax3.boxplot(slow_incorrect_np_arr)
+ax1.boxplot(fast_correct_token_np_arr)
+ax2.boxplot(fast_correct_empty_np_arr)
+ax3.boxplot(fast_incorrect_np_arr)
+plt.savefig('testibox.png', bbox_inches='tight')
+plt.clf()
 
 plt.hist(difference_np_arr[:, 0], 100, facecolor='g', alpha=0.75)
-
 plt.xlabel('Start difference')
 plt.ylabel('#Tokens')
 plt.title('Histogram of start differences')
 plt.xlim(-0.3, 0.3)
 plt.grid(True)
-plt.savefig('testi.png', bbox_inches='tight')
+plt.savefig('testihist.png', bbox_inches='tight')
 
 plt.hist(difference_np_arr[:, 1], 100, facecolor='g', alpha=0.75)
-
 plt.xlabel('End difference')
 plt.ylabel('#Tokens')
 plt.title('Histogram of end differences')
 plt.xlim(-0.3, 0.3)
 plt.grid(True)
-plt.savefig('testi2.png', bbox_inches='tight')
+plt.savefig('testihist2.png', bbox_inches='tight')
+
+print(percentileofscore_of_40ms_start, percentileofscore_of_40ms_end)
