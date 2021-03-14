2bd16b99f6e07ab48435223cae1773592cef3b8e
Juho Leinonen <leinonen@puhti-login1.bullx>
2021-03-14 15:31:59 +0200

    made some changes

--- bin/singularity_wrapper.py
+++ bin/singularity_wrapper.py
@@ -37,55 +37,6 @@ def parse_arguments():
     return args
 
 
-def make_wav(wavpath, tgtpath):
-    wav_name = os.path.join(tgtpath, "data/align/wav.scp")
-    utt2spk_name = os.path.join(tgtpath, "data/align/utt2spk")
-    spk2utt_name = os.path.join(tgtpath, "data/align/spk2utt")
-
-    with open(wav_name, "w", encoding="utf-8") as wavlist_file, \
-            open(utt2spk_name, "w", encoding="utf-8") as utt2spk_file, \
-            open(spk2utt_name, "w", encoding="utf-8") as spk2utt_file:
-
-        listofwavs = []
-        if os.path.isdir(wavpath):
-            listofwavs = sorted(os.listdir(wavpath))
-        elif os.path.isfile(wavpath):
-            listofwavs.append(os.path.split(wavpath)[1])
-            wavpath = os.path.split(wavpath)[0]
-
-        for wavfile in listofwavs:
-            filename, file_extension = os.path.splitext(wavfile)
-            if file_extension == ".wav":
-                wavlist_file.write(filename + " " + os.path.join(wavpath, wavfile) + "\n")
-
-                utt2spk_file.write(filename + " " + filename + "\n")
-                spk2utt_file.write(filename + " " + filename + "\n")
-
-
-def make_txt(txtpath, tgtpath):
-    text_name = os.path.join(tgtpath, "data/align/text")
-    with open(text_name, "w", encoding="utf-8") as text_file:
-        listoftxts = []
-        if os.path.isdir(txtpath):
-            listoftxts = sorted(os.listdir(txtpath))
-        elif os.path.isfile(txtpath):
-            listoftxts.append(os.path.split(txtpath)[1])
-            txtpath = os.path.split(txtpath)[0]
-
-        for txtfile in listoftxts:
-            filename, file_extension = os.path.splitext(txtfile)
-            if file_extension == ".txt":
-                utterances = []
-                with open(os.path.join(txtpath, txtfile), 'r', encoding='utf-8') as utterance_file:
-                    for line in utterance_file:
-                        utterance = line.strip()
-                        if len(utterance) == 0:
-                            continue
-                        utterances.append(utterance)
-
-                text_file.write(filename + " " + " ".join(utterances) + "\n")
-
-
 def check_framerate(wavpath):
     prompt_text = """
     You have an audio file with framerate incompatible with Kaldi ASR. As most speech recognizers 
