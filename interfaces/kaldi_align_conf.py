19266e959760a48423934753492fa88645df3a5d
Martin Matthiesen <martin.matthiesen@csc.fi>
2021-05-12 10:44:30 +0300

    inital conf refactor

---
+++ interfaces/kaldi_align_conf.py
@@ -0,0 +1,5 @@
+#path to singularity container
+container_name = "/tmp/matthies/kaldi-aligner-5.0.sif"
+
+# path to wrapper script
+align_wrapper = "/tmp/matthies/singularity_wrapper.sh"
--- wrappers/recognize_wrapper.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#The whole point of this shell script is that it can be made into slurm easier than python
-
-arguments="$*"
-
-singularity run $arguments 
