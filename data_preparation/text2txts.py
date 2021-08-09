3ff9dd337838a0d762fd220c04e5bf8874bdf3fc
Juho Leinonen <juho.pekka.leinonen@gmail.com>
2021-08-09 15:01:45 +0300

    text2txts with target directory argument

--- data_preparation/text2txts.py
+++ data_preparation/text2txts.py
@@ -8,27 +8,37 @@ import os
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description='Write individual .txt files from kaldi text file')
-    parser.add_argument('name', type=str,
+    parser.add_argument('text_file', type=str,
                         help='Path/Name of the text file')
+    parser.add_argument('--parent_dir', type=str,
+                        help='Path/name to the directory with new txts')
     args = parser.parse_args()
     return args
 
 
-def create_txts_from_text(text_file):
+def create_txts_from_text(text_file, directory=None):
+    text_path = pathlib.Path(text_file)
+    text_parent_dir = text_path.parents
+    if directory == None:
+        target_directory = text_parent_dir[1]
+    else:
+        target_directory = directory
+
     with open(text_file, "r", encoding="utf-8") as text:
         for line in text:
             line = line.strip()
             line_parts = line.split(" ")
             utterance_ID = line_parts[0]
             utterance_text = " ".join(line_parts[1:]) + "\n"
-            with open(utterance_ID + ".txt", "w", encoding="utf-8") as utterance_file:
+            utterance_name = pathlib.Path(utterance_ID).with_suffix(".txt")
+            with open(utterance_name, "w", encoding="utf-8") as utterance_file:
                 utterance_file.write(utterance_text)
 
 
-def main(text_file):
-    create_txts_from_text(text_file)
+def main(arguments):
+    create_txts_from_text(arguments.text_file, arguments.parent_dir)
 
 
 if __name__ == '__main__':
     arguments = parse_arguments()
-    main(arguments.name)
+    main(arguments)
