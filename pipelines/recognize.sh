260e76cbe1afba2bb73d4b01ec40e0833cd0d83e
Juho Leinonen <juho.leinonen@aalto.fi>
2021-09-22 15:38:19 +0300

    kaldi-rec-py2:1.1 update

--- pipelines/recognize.sh
+++ pipelines/recognize.sh
@@ -8,5 +8,15 @@ cd "$project_dir"
 
 arguments="$*"
 
-python kaldi-rec $arguments
+python finnish-forced-alignment/pipelines/kaldi-rec $arguments
 
+# for loop all .ctm files. use basename etc. check previous scripts. if no files works. should not be two files. delete after loop just in case?
+for ctm in *.ctm # should be only one, if there is any
+do
+  python3 finnish-forced-alignment/data_preparation/ctm2results.py $ctm
+done
+
+for txt in *.txt # should be only one, if there is any
+do
+  sed -i 's/NULL: //' $txt
+done
