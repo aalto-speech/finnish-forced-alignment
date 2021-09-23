02962b7ef4dd792ed1e715674e13472d43ffe128
Juho Leinonen <juho.leinonen@aalto.fi>
2021-09-23 17:49:03 +0300

    text NULL fix

--- pipelines/recognize.sh
+++ pipelines/recognize.sh
@@ -10,13 +10,7 @@ arguments="$*"
 
 python finnish-forced-alignment/pipelines/kaldi-rec-py2 $arguments
 
-# for loop all .ctm files. use basename etc. check previous scripts. if no files works. should not be two files. delete after loop just in case?
-for ctm in *.ctm # should be only one, if there is any
-do
-  python3 finnish-forced-alignment/data_preparation/ctm2results.py $ctm
-done
-
-for txt in *.txt # should be only one, if there is any
+for txt in ../../temp/*.txt # should be only one, if there is any
 do
   sed -i 's/NULL: //' $txt
 done
