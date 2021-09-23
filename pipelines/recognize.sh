52acf2c5991c6976b6174d4e5bdef1ea8ea43a41
Juho Leinonen <juho.leinonen@aalto.fi>
2021-09-23 18:26:04 +0300

    NULL and millisecond final fix

--- pipelines/recognize.sh
+++ pipelines/recognize.sh
@@ -12,5 +12,7 @@ python finnish-forced-alignment/pipelines/kaldi-rec-py2 $arguments
 
 for txt in ../../temp/*.txt # should be only one, if there is any
 do
+  if [ -f $txt ]; then
   sed -i 's/NULL: //' $txt
+  fi
 done
