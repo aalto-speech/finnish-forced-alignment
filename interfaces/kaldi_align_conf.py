12d7555b71cfe9c2c11f54f9a313619df26f6b2c
Juho Leinonen <juho.pekka.leinonen@gmail.com>
2021-12-07 10:18:07 +0200

    paths to new container, need better solution soon

--- interfaces/kaldi_align_conf.py
+++ interfaces/kaldi_align_conf.py
@@ -1,5 +1,5 @@
 #path to singularity container
-container_name = "/tmp/matthies/kaldi-aligner-5.0.sif"
+container_name = "/scratch/project_2004354/containers/kaldi-aling_5.2.sif"
 
 # path to wrapper script
-singularity_wrapper = "/tmp/matthies/singularity_wrapper.sh"
+singularity_wrapper = "/scratch/project_2004354/forced-alignment-project/mv_experiments/finnish-forced-alignment/wrappers/singularity_wrapper.sh"
