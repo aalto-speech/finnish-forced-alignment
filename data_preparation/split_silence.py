7b70898dd0923e137d9d65f09672c63088de6a86
Juho Leinonen <juho.leinonen@aalto.fi>
2021-12-27 12:47:12 +0200

    Final split silence

--- data_preparation/split_silence.py
+++ data_preparation/split_silence.py
@@ -2,6 +2,8 @@
 # coding: utf-8
 
 # Imports
+import sys
+import copy
 import pandas
 import argparse
 from analysis.calculate_metrics import create_ctm_dfs  # breaks if run as a script and not -m
@@ -24,40 +26,45 @@ def parse_arguments():
 
 
 def split_silence(created_df, split_type, pause_duration):
-    start_list_ms = (created_df["start"] * 1000).map(int).tolist()
-    duration_list_ms = (created_df["duration"] * 1000).map(int).tolist()
-    end_list_ms = (created_df["end"] * 1000).map(int).tolist()
+    splitted_df = copy.deepcopy(created_df)
 
-    for i in range(len(start_list_ms - 1)):
+    start_list_ms = (splitted_df["start"] * 1000).map(int).tolist()
+    duration_list_ms = (splitted_df["duration"] * 1000).map(int).tolist()
+    end_list_ms = (splitted_df["end"] * 1000).map(int).tolist()
+    pause_duration_ms = int(pause_duration * 1000)
+
+    for i in range(len(start_list_ms) - 1):
         pause = start_list_ms[i + 1] - end_list_ms[i]
-        if 0 < pause < pause_duration:
+        if 0 < pause < pause_duration_ms:
             # Kaldi resolution is 10ms so pause should always be divisible by two
             if split_type == "middle":
-                end_list_ms[i] += pause / 2
-                duration_list_ms[i] += pause / 2
-                duration_list_ms[i + 1] += pause / 2
-                start_list_ms[i + 1] -= pause / 2
-            if split_type == "end":
+                end_list_ms[i] += pause // 2
+                duration_list_ms[i] += pause // 2
+                duration_list_ms[i + 1] += pause // 2
+                start_list_ms[i + 1] -= pause // 2
+            elif split_type == "end":
                 end_list_ms[i] += pause
                 duration_list_ms[i] += pause
-            if split_type == "start":
+            elif split_type == "start":
                 start_list_ms[i + 1] -= pause
                 duration_list_ms[i + 1] += pause
+            else:
+                sys.exit("Error, no split type. Should not be possible to reach this point.")
         else:
             continue  # To spell out the algorithm
 
-    created_df["start"] = [float(i) / 1000 for i in start_list_ms]
-    created_df["duration"] = [float(i) / 1000 for i in duration_list_ms]
-    created_df["end"] = [float(i) / 1000 for i in end_list_ms]
+    splitted_df["start"] = [float(i) / 1000 for i in start_list_ms]
+    splitted_df["duration"] = [float(i) / 1000 for i in duration_list_ms]
+    splitted_df["end"] = [float(i) / 1000 for i in end_list_ms]
 
-    return created_df
+    return splitted_df
 
 
 def main(gold_ctms_file, created_ctms_file, splitted_ctms_file, split_type, pause_duration):
     _, created_ctm_df = create_ctm_dfs(gold_ctms_file, created_ctms_file)
     splitted_df = split_silence(created_ctm_df, split_type, pause_duration)
 
-    splitted_df.to_csv(splitted_ctms_file, index=False, header=None, sep=' ')
+    splitted_df[["Filename", "segment", "start", "duration", "token"]].to_csv(splitted_ctms_file, index=False, header=None, sep=' ')
 
 
 if __name__ == '__main__':
---
+++ pipelines/align_converse_scales.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+# align will be copied from outside
+
+csv_file=$1
+debugBoolean=$2
+textDirBoolean=$3
+src_for_wav=$4
+src_for_txt=$5
+
+project_dir=/opt/kaldi/egs/kohdistus
+src_for_mdl=/opt/kaldi/egs/align
+
+cd "$project_dir"
+
+bin_folder=/opt/kaldi/egs/align/aligning_with_Docker/bin
+
+ln -s ../wsj/s5/utils utils
+ln -s ../wsj/s5/steps steps
+ln -s "$src_for_mdl"/conf conf
+
+mkdir exp
+mkdir -p data/align
+
+ln -s "$src_for_mdl"/exp/nnet3 exp/nnet3
+
+cp ../wsj/s5/path.sh .
+
+sed -i '1 s/...$//' path.sh
+
+mkdir data/dict
+mkdir data/lang
+
+cat <<EOF > data/dict/optional_silence.txt
+SIL
+EOF
+
+cat <<EOF > data/dict/silence_phones.txt
+SIL
+SPN
+NSN
+EOF
+
+cat <<EOF > data/dict/nonsilence_phones.txt
+A
+AE
+B
+D
+E
+F
+G
+H
+I
+J
+K
+L
+M
+N
+O
+OE
+P
+R
+S
+T
+U
+V
+Y
+EOF
+
+if [ "$textDirBoolean" = "textDirTrue" ]
+then
+  python3 $bin_folder/make_wav_and_utt2spk.py "$project_dir" "$src_for_wav" --txtpath "$src_for_txt"
+else
+  python3 $bin_folder/make_wav_and_utt2spk.py "$project_dir" "$src_for_wav"
+fi
+
+sed -i 's/!sil/!SIL/g' data/align/text
+sed -i 's/<unk>/<UNK>/g' data/align/text
+
+cut -f 2- -d ' ' data/align/text > corpus
+
+python3 $bin_folder/change_lex_pho.py corpus $bin_folder/$csv_file
+mv lexicon.txt data/dict
+
+extra=3
+utils/prepare_lang.sh --num-extra-phone-disambig-syms $extra data/dict "<UNK>" data/lang/local data/lang
+
+utils/fix_data_dir.sh data/align
+
+utils/copy_data_dir.sh data/align data/align_hires
+
+nj=1
+
+steps/make_mfcc.sh --nj $nj --mfcc-config conf/mfcc_hires.conf --cmd "run.pl" data/align_hires
+steps/compute_cmvn_stats.sh data/align_hires
+
+utils/fix_data_dir.sh data/align_hires
+
+steps/online/nnet2/extract_ivectors_online.sh --cmd "run.pl" --nj $nj data/align_hires exp/nnet3/extractor exp/align/ivectors_hires
+
+steps/nnet3/align.sh --nj 1 --use_gpu false --scale_opts "--transition-scale=1.0 --acoustic-scale=1.0 --self-loop-scale=1.0" --online_ivector_dir exp/align/ivectors_hires data/align_hires/ data/lang/ exp/nnet3/chain/ exp/align_ali
+
+steps/get_train_ctm.sh data/align_hires data/lang exp/align_ali
+cp exp/align_ali/ct* .
+python3 $bin_folder/ctm2results.py ctm
+
+if [ "$debugBoolean" = "true" ]
+then
+  cp data/align/text .
+  cp data/align/wav.scp .
+  cp data/dict/lexicon.txt .
+fi
+rm corpus path.sh conf steps utils
+rm -r exp data
