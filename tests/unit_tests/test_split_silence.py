ff9baf293c4b469860c5f49cdaf23074ba632654
Juho Leinonen <juho.leinonen@aalto.fi>
2021-12-29 13:13:53 +0200

    init test

--- tests/unit_tests/test_split_silence.py
+++ tests/unit_tests/test_split_silence.py
@@ -1,7 +1,6 @@
-import numpy as np
 import pytest
-from analysis import split_silences
-
+from data_preparation import split_silence
+from analysis import calculate_metrics as c_m
 
 
 @pytest.fixture
@@ -11,21 +10,4 @@ def dfs():
 
 
 def test_split_middle(dfs):
-    errors = []
-    gold_df, created_df = dfs
-    if not gold_df.token[3] == "neljäs":
-        errors.append("tokens not correct")
-    start = gold_df.iloc[-1].start
-    end = gold_df.iloc[-1].end
-    duration = gold_df.iloc[-1].duration
-    if not abs(start + duration - end) < 0.001:
-        errors.append("timestamps not correct")
-
-    assert not errors, "errors occured:\n{}".format("\n".join(errors))
-
-
-def test_calculate_time_from_ctms(dfs, capsys):
-    gold_df, created_df = dfs
-    c_m.calculate_time_from_ctms(gold_df, created_df)
-    captured = capsys.readouterr()
-    assert captured.out[:40] == "start is 0.4, end is 4.2 and total: 3.80"
+    pass
